bc26e787acb7e37ce0985b945b423b60b9314dcf
Joel Gregory <joel@vistametalsgeorgia.com>
2016-08-23 09:41:44 -0400

    Created constructor

--- card.py
+++ card.py
@@ -4,3 +4,9 @@ class card():
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
+        if(suit == "spades" or suit == "clubs"):
+            self.color = "black"
+        else:
+            self.color = "red"
+
+    
